6017a9eb21a4ef95a74859e3d9d8ac11e668a0d1
sapiezynski <piotr@sapiezynski.eu>
2013-07-02 11:39:38 +0200

    Added support for grid questions

--- mysite/questionaires/formwidget.py
+++ mysite/questionaires/formwidget.py
@@ -10,11 +10,17 @@ class Formwidget:
 		self.inclusion_condition = inclusion_condition;
 		self.answer_type = answer_type;
 		self.variable_name = variable_name;
-		self.answers = answers;
+		answers = answers.split(";");
+		self.answers = [];
+		for answer in answers:
+			self.answers.append(answer.strip());
 		self.extra_param = extra_param;
-
+		self.data = [];
 	def render(self):
 		return "<p></p>";
+
+	def to_html(self):
+		return '<div class="formwidget">\n' + self.render() + '\n</div>';
 
 def htmlize(string):
 	return re.sub('[^a-z_]','',string.strip().lower().replace(' ','_'));
@@ -39,20 +45,53 @@ class Header(Formwidget):
 class RadioQuestion(Question):
 	def render(self):
 		resp = self.prerender();
-		answers_parts = self.answers.split(";");
-		for answer in answers_parts:
+		for answer in self.answers:
 			resp += '<input type="radio" name="' + self.variable_name + \
-					'" value="' + htmlize(answer) + '"/>' + answer.strip() + '<br />\n'
+					'" value="' + htmlize(answer) + '"/>' + answer + '<br />\n'
 		return resp;
 
 class ListQuestion(Question):
 	def render(self):
 		resp = self.prerender() + '\n<select>\n';
-		answers_parts = self.answers.split(";");
-		for answer in answers_parts:
-	 		resp += '\t<option value="' + htmlize(answer) + '">' + answer.strip() + '</option>\n'
+		for answer in self.answers:
+	 		resp += '\t<option value="' + htmlize(answer) + '">' + answer + '</option>\n'
 		resp += '<select>\n';
 		
 class NumberQuestion(Formwidget):
 	def render(self):
 		resp = self.prerender() + '\n<input type="number" name="' + htmlize(self.variable_name) + '" />'
+
+class SubQuestion(Formwidget):
+	def render(self):
+		resp = '\n<tr>\n\t<td>' + self.secondary_content + '</td>\n'
+		if self.answer_type == 'radio':
+			for answer in self.answers:
+				resp += '\t<td><input type="radio" name="' + htmlize(self.variable_name) + '" value="' + htmlize(answer) + '"></td>\n'
+		
+		elif self.answer_type == 'number':
+			for answer in self.answers:
+				resp += '\t<td><input type="number" name="' + self.variable_name + '_' + htmlize(answer) + '" /></td>\n'
+
+		resp += '</tr>';
+		return resp;
+		
+		
+class GridQuestion(Question):
+
+	def add_subquestion(self, subquestion):
+		self.data.append(subquestion)
+
+
+	def render(self):
+		resp = self.prerender() + '\n<table>\n';
+		resp += '<tr>\n\t<th></th>';
+		for answer in self.answers:
+			resp +='\n\t<th>' + answer + '</th>';
+		resp += '\n</tr>\n'
+		for sub in self.data:
+			resp += sub.render();
+
+		resp += '\n</table>';
+
+		return resp
+		
