bebf7eceec84ae1a096a06f9ee2825040e6db6bf
Arek Stopczynski <arekquest@gmail.com>
2013-07-05 20:45:14 +0000

    oauth2 models

--- questionnaires/utils/models.py
+++ questionnaires/utils/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Scope(models.Model):
+        scope = models.CharField(unique=True, max_length=100, db_index=True)
+        description = models.TextField(blank=True)
+        def __unicode__(self):
+                return self.scope
+
+class AccessToken(models.Model):
+	user = models.ForeignKey(User)
+	token = models.CharField(unique=True, max_length=100, db_index=True)
+    	refresh_token = models.CharField(unique=True, blank=True, null=True, max_length=100, db_index=True)
+	scope = models.ManyToManyField(Scope)
