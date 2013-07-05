3e0cb630bf70cef3dbe895c3b542b39eb7733fad
Arek Stopczynski <arekquest@gmail.com>
2013-07-05 19:57:58 +0000

    hello world view

--- questionnaires/render/views.py
+++ questionnaires/render/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+	user = request.user
+	return HttpResponse(user.email)
