76c94cf208b396bad302685604090508a7043b54
Arek Stopczynski <arekquest@gmail.com>
2013-07-05 20:44:39 +0000

    first views

--- questionnaires/render/views.py
+++ questionnaires/render/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from utils import authorizations
+from django.shortcuts import render_to_response
 
 @login_required
 def home(request):
 	user = request.user
-	return HttpResponse(user.email)
+	auth = authorizations.getUserAuthorization(user)
+	if auth == None:
+		#show user site to authorize the form
+		return render_to_response('start_auth.html', {})
+	
+	return render_to_response('form.html', {})
+		
