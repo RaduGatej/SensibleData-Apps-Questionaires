6643d37fd2927ef3276582035d3e14de87f898dd
Arek Stopczynski <arekquest@gmail.com>
2013-07-05 23:18:34 +0000

    expanding render

--- questionnaires/render/views.py
+++ questionnaires/render/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from utils import authorizations
 from django.shortcuts import render_to_response
+import formwidget
 
 @login_required
 def home(request):
@@ -9,7 +10,9 @@ def home(request):
 	auth = authorizations.getUserAuthorization(user)
 	if auth == None:
 		#show user site to authorize the form
-		return render_to_response('start_auth.html', {})
+		return render_to_response('start_auth.html', {'username': request.user.email})
 	
-	return render_to_response('form.html', {})
+	#TODO: check which questions has been answered already and do not include them
+	return render_to_response('form.html', {'username': request.user.email})
+	#return HttpResponse(formwidget.parsefile('/home/arks/MODIS/SensibleData-Apps-Questionaires/questionnaires/render/data/sample_new.txt')[0].render())
 		
