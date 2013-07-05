d02da2b18621a5f03178927d14bce350c1909add
Arek Stopczynski <arekquest@gmail.com>
2013-07-05 23:19:00 +0000

    added oauth2 state

--- questionnaires/utils/admin.py
+++ questionnaires/utils/admin.py
@@ -6,12 +6,14 @@ class ScopeAdmin(admin.ModelAdmin):
         class Meta:
                 verbose_name = 'scope'
 
-
 admin.site.register(Scope, ScopeAdmin)
 
 class AccessTokenAdmin(admin.ModelAdmin):
         list_display = ('user', 'token')
 
-
 admin.site.register(AccessToken, AccessTokenAdmin)
 
+class StateAdmin(admin.ModelAdmin):
+        list_display = ('user', 'nonce')
+
+admin.site.register(State, StateAdmin)
--- questionnaires/utils/models.py
+++ questionnaires/utils/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+import time
+from hashlib import sha512
+from uuid import uuid4
+
+class TimestampGenerator(object):
+    def __init__(self, seconds=0):
+        self.seconds = seconds
+
+    def __call__(self):
+        return int(time.time()) + self.seconds
+
+class KeyGenerator(object):
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self):
+        return sha512(uuid4().hex).hexdigest()[0:self.length]
+
 
 class Scope(models.Model):
         scope = models.CharField(unique=True, max_length=100, db_index=True)
@@ -12,3 +30,7 @@ class AccessToken(models.Model):
 	token = models.CharField(unique=True, max_length=100, db_index=True)
     	refresh_token = models.CharField(unique=True, blank=True, null=True, max_length=100, db_index=True)
 	scope = models.ManyToManyField(Scope)
+
+class State(models.Model):
+	user = models.ForeignKey(User)
+	nonce = models.CharField(unique=True, max_length=100, db_index=True, default=KeyGenerator(15))
