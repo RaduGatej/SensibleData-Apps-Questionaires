ef0af8f78ab65af12c308d8cfd43f65e20908018
Arek Stopczynski <arekquest@gmail.com>
2013-07-06 19:16:56 +0000

    forcing logout from idp

--- questionnaires/utils/logout.py
+++ questionnaires/utils/logout.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+import SECURE_CONFIG
 
-@login_required
 def do_logout(request):
-	logout(request)
+	if request.user.is_authenticated():
+		logout(request)
+		return redirect(SECURE_CONFIG.IDP_URI+'accounts/logout/?next='+SECURE_CONFIG.APPLICATION_URI+'logout/')	
 	return redirect('logout_success')	
