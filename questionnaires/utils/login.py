52aa25f8caad44e096898179565782f97e77b55c
Arek Stopczynski <arekquest@gmail.com>
2013-07-08 13:22:09 +0000

    first login logic

--- questionnaires/utils/login.py
+++ questionnaires/utils/login.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
-
+from .models import *
 
 @login_required
 def do_login(request):
-        return redirect('home')
+	try: f = FirstLogin.objects.get(user=request.user)
+	except FirstLogin.DoesNotExist: f = FirstLogin.objects.create(user=request.user)
+	if f.firstLogin:
+		f.firstLogin = False
+		f.save()
+		return redirect('request_attributes')
+	return redirect('home')
