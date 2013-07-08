be6acb56d50177b6b2b87a02ac9436f2ab220e88
Arek Stopczynski <arekquest@gmail.com>
2013-07-08 13:23:04 +0000

    tweak in authorization

--- questionnaires/utils/authorizations.py
+++ questionnaires/utils/authorizations.py
@@ -2,7 +2,7 @@ from .models import *
 
 def getUserAuthorization(user):
 	try:
-		auth = AccessToken.objects.get(user=user)
+		auth = AccessToken.objects.get(user=user, type='data')
 	except AccessToken.DoesNotExist:
 		auth = None
 	return auth
