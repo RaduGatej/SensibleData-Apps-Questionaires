bf9bf949f4f5e0625cfb27388f1524eee41afe01
Arek Stopczynski <arekquest@gmail.com>
2013-07-08 13:19:45 +0000

    added openid failed

--- questionnaires/render/urls.py
+++ questionnaires/render/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
+    url(r'^openid_failed', 'render.views.openid_failed', name='openid_failed'),
     url(r'^logout_success', 'render.views.logout_success', name='logout_success'),
     url(r'^form', 'render.views.form', name='form'),
     url(r'^', 'render.views.home', name='home'),
--- questionnaires/render/views.py
+++ questionnaires/render/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from utils import authorizations
+from utils import oauth2, identity
+from utils.models import Scope
 from django.shortcuts import render_to_response, redirect
 import formwidget
 from django.template import RequestContext
@@ -10,7 +11,8 @@ def home(request):
 
 @login_required
 def form(request):
-	auth = authorizations.getUserAuthorization(request.user)
+	#TODO add wrapper function for gettting authorizations
+	auth = oauth2.getToken(request.user, scope=Scope.objects.get(scope='connector_questionnaire.input_form_data'))
 	if auth == None:
 		#show user site to authorize the form
 		return render_to_response('start_auth.html', {}, context_instance=RequestContext(request))
@@ -18,7 +20,11 @@ def form(request):
 
 @login_required
 def login(request):
+	getAttributes(request.user, ['email', 'first_name'])
 	return redirect('home')
 
 def logout_success(request):
 	return render_to_response('logout_success.html', {}, context_instance=RequestContext(request))
+
+def openid_failed(request):
+	return render_to_response('openid_failed.html', {}, context_instance=RequestContext(request))
