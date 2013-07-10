e64760a39eab17494ccea6c9c375d30a6d02e981
Arek Stopczynski <arekquest@gmail.com>
2013-07-10 16:00:38 +0000

    fixing commits

--- questionnaires/render/views.py
+++ questionnaires/render/views.py
@@ -17,13 +17,6 @@ def form(request):
 	#if auth == None:
 		#show user site to authorize the form
 	#	return render_to_response('start_auth.html', {}, context_instance=RequestContext(request))
-<<<<<<< HEAD
-<<<<<<< HEAD
-	#pdb.set_trace();
-=======
->>>>>>> 0d3dcd11d846cb81e8f6109db6107331ce03fa25
-=======
->>>>>>> 0d3dcd11d846cb81e8f6109db6107331ce03fa25
 	next_question = None;
 	unanswered = False;
 	if request.POST:
@@ -65,13 +58,6 @@ def form(request):
 	else:	
 		di['question'] = next_question.to_html()
 		di['unanswered'] = unanswered
-<<<<<<< HEAD
-<<<<<<< HEAD
-	#pdb.set_trace()
-=======
->>>>>>> 0d3dcd11d846cb81e8f6109db6107331ce03fa25
-=======
->>>>>>> 0d3dcd11d846cb81e8f6109db6107331ce03fa25
 	return render_to_response('form.html', di, context_instance=RequestContext(request))
 
 @login_required
