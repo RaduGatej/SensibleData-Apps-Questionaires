dc3ddab64d6d8ba345d5bf5602e678bfc1ca305c
sapiezynski <piotr@sapiezynski.eu>
2013-07-10 16:43:59 +0200

    navigation updates
    Added "Previous","Next" and "Next unanswered" buttons to the form pages
    and "Start from the beginning" to the last page

--- questionnaires/render/views.py
+++ questionnaires/render/views.py
@@ -20,34 +20,47 @@ def form(request):
 		#show user site to authorize the form
 	#	return render_to_response('start_auth.html', {}, context_instance=RequestContext(request))
 	pdb.set_trace();
-	# add answers:
-	answers = [];
-	required_vars = [];
+	next_question = None;
 	unanswered = False;
-	for ans in request.POST.keys():
-		if ans == 'required_vars': # list of required answers
-			required_vars = request.POST[ans].split(",");
-		elif ans == 'csrfmiddlewaretoken': # token
-			continue
-		elif request.POST[ans] == '': #empty answer
-			unanswered = True;
-		else: #good answer
-			answers.append(ans);
-			form_provider.set_answer(request.user, '1.0', ans, request.POST[ans]);
-	if len(required_vars) > 0:
-		for v in required_vars:
-			if v not in answers:
-				unanswered = True;
-				break
-		
-	
-	next_question = form_provider.get_next_unanswered_question(request.user,'1.0');
-	if next_question is None:
-		return render_to_response('form_last.html', {}, context_instance=RequestContext(request))
-	
+	if request.POST:
+		if '_prev' in request.POST:
+			next_question = form_provider.get_previous_question(request.user,'1.0',request.POST['__question_name']);
+		elif '_from_top' in request.POST:
+			next_question = form_provider.get_first_question(request.user,'1.0');
+		else:
+			# add answers:
+			answers = [];
+			required_vars = [];
+			for ans in request.POST.keys():
+				if ans == '__required_vars': # list of required answers
+					required_vars = request.POST[ans].split(",");
+				elif (ans == str('csrfmiddlewaretoken')) | (ans == str('__question_name')): # token or question_name
+					continue
+				elif request.POST[ans] == '': #empty answer
+					continue
+				else: #good answer
+					answers.append(ans);
+					form_provider.set_answer(request.user, '1.0', ans, request.POST[ans]);
+			if len(required_vars) > 0:
+				for v in required_vars:
+					if v not in answers:
+						unanswered = True;
+						break
+				
+			if '_next' in request.POST:
+				next_question = form_provider.get_next_question(request.user,'1.0',request.POST['__question_name']);
+			elif '_next_new' in request.POST:
+				next_question = form_provider.get_next_unanswered_question(request.user,'1.0');	
+	else:
+		next_question = form_provider.get_next_unanswered_question(request.user,'1.0');
+
 	di = {}
-	di['question'] = form_provider.get_next_unanswered_question(request.user,'1.0').to_html()
-	di['unanswered'] = unanswered
+	if next_question is None:
+		di['unanswered'] = False;
+		di['last_page'] = True;
+	else:	
+		di['question'] = next_question.to_html()
+		di['unanswered'] = unanswered
 	pdb.set_trace()
 	return render_to_response('form.html', di, context_instance=RequestContext(request))
 
