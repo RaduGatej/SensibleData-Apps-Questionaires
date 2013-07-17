0647ed26614e99b43ecff27fb423caf0f871b225
h0pbeat <arekquest@gmail.com>
2013-07-17 14:03:27 +0000

    added about and quit endpoints

--- questionnaires/render/urls.py
+++ questionnaires/render/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     url(r'^openid_failed', 'render.views.openid_failed', name='openid_failed'),
     url(r'^logout_success', 'render.views.logout_success', name='logout_success'),
     url(r'^form', 'render.views.form', name='form'),
+    url(r'^about', 'render.views.about', name='about'),
+    url(r'^quit', 'render.views.quit', name='quit'),
     url(r'^', 'render.views.home', name='home'),
 )
