1e683f22e622f48e965ab600ecd095860060b701
h0pbeat <arekquest@gmail.com>
2013-08-04 16:06:45 +0000

    moved to sensible/ prefix

--- questionnaires/render/urls.py
+++ questionnaires/render/urls.py
@@ -5,13 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^nochanges','render.views.nochanges',name='changebrowser'),
-	url(r'^changebrowser','render.views.changebrowser',name='changebrowser'),
-	url(r'^noscript','render.views.noscript', name='noscript'),
-    url(r'^openid_failed', 'render.views.openid_failed', name='openid_failed'),
-    url(r'^logout_success', 'render.views.logout_success', name='logout_success'),
     url(r'^form', 'render.views.form', name='form'),
     url(r'^about', 'render.views.about', name='about'),
-    url(r'^quit', 'render.views.quit', name='quit'),
     url(r'home_refreshed', 'render.views.home_refreshed', name='home_refreshed'),
     url(r'^', 'render.views.home', name='home'),
 )
