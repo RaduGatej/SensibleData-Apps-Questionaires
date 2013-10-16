168b730b937db4061543b1f6c404082e4ef3b75b
root <root@ip-172-31-42-85.eu-west-1.compute.internal>
2013-10-16 14:20:49 +0000

    added Survey model

--- questionnaires/render/models.py
+++ questionnaires/render/models.py
@@ -6,13 +6,23 @@ class Response(models.Model):
 	form_version = models.CharField(max_length=64)
 	variable_name = models.CharField(max_length=1024)
 	response = models.CharField(max_length=1024)
-	last_answered = models.DateTimeField(auto_now=True)
+	last_answered = models.DateTimeField(auto_now_add=True)
+	#last_answered = models.DateTimeField(auto_now=True)
 	synced_with_study = models.BooleanField(default=False)
+	human_readable_question = models.CharField(max_length=4096)
+	human_readable_response = models.CharField(max_length=1024)
 
 	@property
 	def details( self ):
 		return repr( dict( user_id=self.user_id, form_version=self.form_version, variable_name=self.variable_name,\
 							response = self.response, last_answered=self.last_answered ) )
+class Survey(models.Model):
+	form_version = models.CharField(max_length=32)
+	created = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=1024)
+	content = models.TextField(max_length=pow(2,20)) # 1 MB
+	prerequisite = models.CharField(max_length=1024)
+
 
 #class Progress(models.Model):
 #	user_id = models.CharField(max_length=64);
