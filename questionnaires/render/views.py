4658b2784da83c94829f7166d9c0fac5db2c857e
sapiezynski <sapiezynski@gmail.com>
2015-09-26 16:30:28 -0700

    adding some sorcery for recognizing children and parents

--- questionnaires/render/views.py
+++ questionnaires/render/views.py
@@ -61,6 +61,10 @@ def form(request):
 	#pdb.set_trace()
 	if 'type_id' in request.GET.keys():
 		request.session['type_id'] = request.GET.get('type_id')
+		try:
+			ischild = request.esssion['type_id'].startswith('child')
+		except:
+			pass
 	if settings.DO_AUTH:
 		auth = oauth2.getToken(request.user, 'connector_questionnaire.input_form_data')
 		if auth == None:
